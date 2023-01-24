8cf20bb971078490ff96ae9daf0e41049d1d337a
Arnaud CLAVIER <clavier.a18@gmail.com>
2023-01-24 13:25:18 +0100

    Effets des boutons et récupération des données login

--- scripts/login.js
+++ scripts/login.js
@@ -1,47 +1,37 @@
-/*const login = await fetch("http://localhost:5678/api/users/login", {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8'
-    },
-    body: JSON.stringify ({
-        "email": document.querySelector("#connectionEmail"),
-        "password": document.querySelector("#password")
-    })
-  });*/
-
-  async function loginFetch(data, url) {
-    const monHeader = new Headers({
-      'Content-Type': 'application/json;charset=utf-8',
-      'accept': 'application/json'
-    }) 
-    const httpOptions = {
-      method : "POST",
-      headers : monHeader,
-      body : JSON.stringify(data)
-    }
-    const reponse = await fetch(url, httpOptions)
-    return reponse
+async function loginFetch(data, url) {
+  const myHeader = new Headers({
+    'Content-Type': 'application/json;charset=utf-8',
+    'accept': 'application/json'
+  })
+  const postHttpOptions = {
+    method: "POST",
+    headers: myHeader,
+    body: JSON.stringify(data)
   }
+  const reponse = await fetch(url, postHttpOptions)
+  return reponse
+}
 
 
 
 document
-    .getElementById("connectionForm").onsubmit = async (event) => {
-      event.preventDefault()
-      let form = document.getElementsByTagName("form")
-      let formData = new FormData(form[0])
-      const user = {
-        "email" : formData.get("email"),
-        "password" : formData.get("password")
-      }
-      console.log(user)
-      
-      try {
-        const reponse = await loginFetch(user, "http://localhost:5678/api/users/login")
-        console.log(reponse)
-      } catch (error) {
-        console.log({
-          message : error
-        })
-      }
+  .getElementById("connectionForm").onsubmit = async (event) => {
+    event.preventDefault()
+    let form = document.getElementsByTagName("form")
+    let formData = new FormData(form[0])
+    const user = {
+      "email": formData.get("email"),
+      "password": formData.get("password")
     }
+
+    try {
+      const response = await loginFetch(user, "http://localhost:5678/api/users/login")
+        .then(response => response.json())
+        .then(data => console.log(data))
+    }
+    catch (error) {
+      console.log({
+        message: error
+      })
+    }
+  }
