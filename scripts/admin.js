ae11facffc1e826f707470558769a50ca4145e5f
Arnaud CLAVIER <clavier.a18@gmail.com>
2023-02-09 14:59:03 +0100

    Creation Modal

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,2 +1,9 @@
-const token = localStorage.getItem("token")
+let token = localStorage.getItem("token")
 console.log(token)
+
+if (token != null ) {
+    document.querySelectorAll(".edition").style.display = "none";
+} else {
+    document.querySelectorAll(".edition").style.display = "flex";
+};
+
--- scripts/login.js
+++ scripts/login.js
@@ -1,4 +1,4 @@
-async function loginFetch(data, url) {
+/*async function loginFetch(data, url) {
   const myHeader = new Headers({
     'Content-Type': 'application/json;charset=utf-8',
     'accept': 'application/json'
@@ -8,11 +8,9 @@ async function loginFetch(data, url) {
     headers: myHeader,
     body: JSON.stringify(data)
   }
-  const reponse = await fetch(url, postHttpOptions)
-  return reponse
+  const response = await fetch(url, postHttpOptions)
+  return response
 }
-
-
 
 document
   .getElementById("connectionForm").onsubmit = async (event) => {
@@ -34,4 +32,45 @@ document
         message: error
       })
     }
+  }*/
+
+async function login(data, url) {
+  const postHttpOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+      'accept': 'application/json'
+    },
+    body: JSON.stringify(data)
   }
+  let response = await fetch(url, postHttpOptions);
+  return response
+}
+
+const token = undefined;
+
+document
+  .getElementById("connectionForm").onsubmit = async (event) => {
+    event.preventDefault()
+    let form = document.getElementsByTagName("form")
+    let formData = new FormData(form[0])
+    const user = {
+      "email": formData.get("email"),
+      "password": formData.get("password")
+    }
+
+    try {
+      const response = await login(user, "http://localhost:5678/api/users/login")
+        .then(response => response.json())
+        .then(data => localStorage.setItem("token", data.token))
+        window.location.href = "index.html"
+    }
+
+    catch (error) {
+      console.log({
+        message: error
+      })
+    }
+
+  }
+
