825d3d3d608feebf8b5c45d3510755e58eba0270
Arnaud CLAVIER <clavier.a18@gmail.com>
2023-02-14 00:21:05 +0100

    Ajout du logout

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,9 +1,26 @@
 let token = localStorage.getItem("token")
 console.log(token)
 
-if (token != null ) {
-    document.querySelectorAll(".edition").style.display = "none";
-} else {
-    document.querySelectorAll(".edition").style.display = "flex";
+/*Affichage des éléments admin*/
+
+function admin() {
+    document.querySelector("#editionBar").style.display = "flex";
+    document.querySelector("#pictureEdition").style.display = "flex";
+    document.querySelector("#introEdition").style.display = "flex";
+    document.querySelector("#projectEdition").style.display = "flex";
+    document.querySelector("#loginButton").style.display = "none";
+    document.querySelector("#logoutButton").style.display = "flex";
 };
 
+if (token != null ) {
+    admin();
+};
+
+/*Boutton de déconnexion*/
+
+function logout() {
+    localStorage.removeItem("token");
+    window.location.href = "index.html";
+}
+
+document.querySelector("#logoutButton").addEventListener("click", logout);
--- scripts/login.js
+++ scripts/login.js
@@ -1,39 +1,3 @@
-/*async function loginFetch(data, url) {
-  const myHeader = new Headers({
-    'Content-Type': 'application/json;charset=utf-8',
-    'accept': 'application/json'
-  })
-  const postHttpOptions = {
-    method: "POST",
-    headers: myHeader,
-    body: JSON.stringify(data)
-  }
-  const response = await fetch(url, postHttpOptions)
-  return response
-}
-
-document
-  .getElementById("connectionForm").onsubmit = async (event) => {
-    event.preventDefault()
-    let form = document.getElementsByTagName("form")
-    let formData = new FormData(form[0])
-    const user = {
-      "email": formData.get("email"),
-      "password": formData.get("password")
-    }
-
-    try {
-      const response = await loginFetch(user, "http://localhost:5678/api/users/login")
-        .then(response => response.json())
-        .then(data => console.log(data))
-    }
-    catch (error) {
-      console.log({
-        message: error
-      })
-    }
-  }*/
-
 async function login(data, url) {
   const postHttpOptions = {
     method: 'POST',
