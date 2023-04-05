2ae8e9295380dbe8cd961ffdeb1987d3d3d8e9b8
Arnaud CLAVIER <clavier.a18@gmail.com>
2023-04-05 02:50:49 +0200

    Validation du formulaire

---
+++ scripts/form.js
@@ -0,0 +1,14 @@
+/*import { currentlyDisplayImage } from "./modal.js";
+
+const addImageForm = document.querySelector("#addImageForm");
+addImageForm.addEventListener("change", formValidation);
+
+function formValidation() {
+    const titleRegex = /[A-Za-z.&",-\d\s]{2,40}/;
+    if (currentlyDisplayImage) {
+        console.log("ca marche")
+    }
+}
+
+let formProject = new FormData();
+*/
--- scripts/modal.js
+++ scripts/modal.js
@@ -8,15 +8,15 @@ let modalContainer = document.querySelector("#modalContainer");
 const openGalleryModal = function (e) {
     modalContainer.innerHTML =
         '<aside id="projectModal" class="modal" aria-modal="true" aria-labelledby="modalTitle">'
-        + '<div id="modalWrapper" class="modalStop">'
+        + '<div id="modalWrapper">'
         + '<div id="modalButton">'
         + '<button id="modalClose"><i class="fa-solid fa-xmark"></i></button>'
         + '</div>'
         + '<h1 id="modalTitle">Galerie photo</h1>'
         + '<div id="editionGallery">'
         + '</div>'
-        + '<button id="addProject">Ajouter une photo</button>'
-        + '<button id="deleteGallery">Supprimer la galerie</button>'
+        + '<button id="addProjectButton">Ajouter une photo</button>'
+        + '<button id="deleteGalleryButton">Supprimer la galerie</button>'
         + '</div>'
         + '</aside>';
 
@@ -25,24 +25,15 @@ const openGalleryModal = function (e) {
     document.querySelector("#projectModal").addEventListener("click", closeGalleryModal);
     document.querySelector("#modalWrapper").addEventListener("click", stopPropagation);
     document.querySelector("#modalClose").addEventListener("click", closeGalleryModal);
-    document.querySelector("#addProject").addEventListener("click", openAddProjectModal);
-    window.addEventListener("keydown", function (e) {
-        if (e.key === "Escape" || e.key === "Esc") {
-            closeGalleryModal(e)
-        }
-    });
+    document.querySelector("#addProjectButton").addEventListener("click", openAddProjectModal);
 }
+
 
 const closeGalleryModal = function () {
     document.querySelector("#projectModal").removeEventListener("click", closeGalleryModal);
     document.querySelector("#modalWrapper").removeEventListener("click", stopPropagation);
     document.querySelector("#modalClose").removeEventListener("click", closeGalleryModal);
-    document.querySelector("#addProject").removeEventListener("click", openAddProjectModal);
-    window.removeEventListener("keydown", function (e) {
-        if (e.key === "Escape" || e.key === "Esc") {
-            closeGalleryModal(e)
-        }
-    });
+    document.querySelector("#addProjectButton").removeEventListener("click", openAddProjectModal);
     modalContainer.innerHTML = "";
 }
 
@@ -57,11 +48,15 @@ const generateProjectsEdition = function (works) {
         imageElement.src = projets.imageUrl;
         imageElement.alt = projets.title;
         imageElement.crossOrigin = "anonymous"
+        const deleteElement = document.createElement("button");
+        deleteElement.className = "imageDeleteButton";
+        deleteElement.innerHTML = '<i class="fa-solid fa-trash-can"></i>'
         const captionElement = document.createElement("figcaption");
         captionElement.innerText = "éditer";
 
         editionGallery.appendChild(element);
         element.appendChild(imageElement);
+        element.appendChild(deleteElement);
         element.appendChild(captionElement);
     }
 }
@@ -78,23 +73,30 @@ document.querySelector("#projectEdition").addEventListener("click", openGalleryM
 const openAddProjectModal = function () {
     modalContainer.innerHTML =
         '<aside id="projectModal" class="modal" aria-labelledby="modalTitle">'
-        + '<div id="modalWrapper" class="modalStop">'
+        + '<div id="modalWrapper">'
         + '<div id="modalButton">'
         + '<button id="returnGallery"><i class="fa-solid fa-arrow-left"></i></button>'
         + '<button id="modalClose"><i class="fa-solid fa-xmark"></i></button>'
         + '</div>'
         + '<h1 id="modalTitle">Ajout photo</h1>'
-        + '<form>'
+        + '<form id="addImageForm">'
         + '<div id="addImageSection">'
+        + '<div id="imageInformation">'
         + '<i class="fa-regular fa-image"></i>'
-        + '<label for="file" id="label-file">+ Ajout photo</label>'
-        + '<input type="file" id="file">'
+        + '</div>'
+        + '<label for="fileInput" id="fileInputLabel">+ Ajout photo</label>'
+        + '<input type="file" id="fileInput" accept="image/jpeg, image/png">'
         + '<p>jpg.png : 4mo max</p>'
         + '</div>'
-        + '<label for="title" id="addImageRessourceLabel">Titre</label>'
+        + '<label for="title" class="addImageRessourceLabel">Titre</label>'
         + '<input type="text" name="title" id="addImageTitle" class="addImageRessource">'
-        + '<label for="category" id="addImageRessourceLabel">Catégorie</label>'
-        + '<input type="list" name="category" id="addImageCategory" class="addImageRessource">'
+        + '<label for="category" class="addImageRessourceLabel">Catégorie</label>'
+        + '<select name="category" class="addImageRessource" id="addImageCategory">'
+        + '<option value=""></option>'
+        + '<option value="objects">Objets</option>'
+        + '<option value="appartements">Appartements</option>'
+        + '<option value="hotelsAndRestaurants">Hôtels & restaurants</option>'
+        + '</select>'
         + '<button id="addImageValidation">Valider</button>'
         + '</form>'
         + '</div>'
@@ -104,23 +106,80 @@ const openAddProjectModal = function () {
     document.querySelector("#modalWrapper").addEventListener("click", stopPropagation)
     document.querySelector("#returnGallery").addEventListener("click", openGalleryModal)
     document.querySelector("#modalClose").addEventListener("click", closeAddProjectModal)
-    window.addEventListener("keydown", function (e) {
-        if (e.key === "Escape" || e.key === "Esc") {
-            closeAddProjectModal(e)
-        }
-    })
+    document.querySelector("#fileInput").addEventListener("change", previewFile);
+    document.querySelector("#addImageForm").addEventListener("change", formValidation)
 };
+
+let currentlyDisplayImage = false;
+export { currentlyDisplayImage };
 
 const closeAddProjectModal = function (e) {
     document.querySelector("#projectModal").removeEventListener("click", closeAddProjectModal);
     document.querySelector("#modalWrapper").removeEventListener("click", stopPropagation);
     document.querySelector("#returnGallery").removeEventListener("click", openGalleryModal);
     document.querySelector("#modalClose").removeEventListener("click", closeAddProjectModal);
-    window.removeEventListener("keydown", function (e) {
-        if (e.key === "Escape" || e.key === "Esc") {
-            closeAddProjectModal(e)
-        }
-    });
+    if (currentlyDisplayImage = false) {
+        document.querySelector("#fileInput").removeEventListener("change", previewFile);
+    }
+    document.querySelector("#addImageForm").removeEventListener("change", formValidation)
     modalContainer.innerHTML = "";
 }
 
+/*Prévision de l'image*/
+
+function previewFile() {
+    const fileExtensionRegex = /\.(jpe?g|png)$/i;
+    if (fileExtensionRegex.test(this.files[0].name)) {
+        if ((this.files[0].size) <= 4194304) {
+            document.querySelector("#addImageSection").innerHTML = ""
+            const file = this.files[0];
+            const fileReader = new FileReader();
+            fileReader.readAsDataURL(file);
+            fileReader.addEventListener("load", (event) => displayImage(event, file));
+            currentlyDisplayImage = true;
+        } else {
+            let wrongSize = document.createElement("span");
+            wrongSize.className = "wrongFileText"
+            wrongSize.innerText = "Cette image est trop grande";
+            document.querySelector("#imageInformation").innerHTML = ""
+            document.querySelector("#imageInformation").appendChild(wrongSize);
+        }
+    } else {
+        let wrongType = document.createElement("span");
+        wrongType.className = "wrongFileText"
+        wrongType.innerText = "Ce fichier n'est pas au bon format";
+        document.querySelector("#imageInformation").innerHTML = ""
+        document.querySelector("#imageInformation").appendChild(wrongType);
+    }
+}
+
+function displayImage(event, file) {
+    const figureElement = document.createElement("figure");
+    figureElement.id = "selectedImage";
+
+    const imageElement = document.createElement("img");
+    imageElement.src = event.target.result;
+
+    figureElement.appendChild(imageElement);
+
+    document.querySelector("#addImageSection").appendChild(figureElement);
+}
+
+/*Validation du formulaire*/
+
+let validForm = false
+
+function formValidation() {
+    const validationButton = document.querySelector("#addImageValidation");
+    const title = document.querySelector("#addImageTitle").value;
+    const category = document.querySelector("#addImageCategory").value;
+    const titleRegex = /[A-Za-z.&"',-\d\s]{3,40}/;
+    if (currentlyDisplayImage) {
+        if (titleRegex.test(title)) {
+            if (category != "") {
+                validationButton.style.backgroundColor = "#1D6154"
+                validForm = true
+            }
+        }
+    }
+}
